d4dacb686c6df034a06af1b9c89301ba3d610b4e
foo@bar.com <edakksm>
2018-11-07 15:31:49 -0500

    Updated pos neg users

--- Process.py
+++ Process.py
@@ -6,6 +6,7 @@ import json
 from dateutil.relativedelta import relativedelta
 from dateutil import parser
 from Init import Init
+from collections import defaultdict
 
 init = Init()
 def Process():
@@ -13,41 +14,42 @@ def Process():
     non_endo_subreddits = pd.read_csv('statistics.csv').iloc[:,0]
     start_date = parser.parse("Oct 01 2017 12:00AM")
     init_date = parser.parse('Apr 01 2017 12:00AM')
-    pos_users = []
-    neg_users = []
+    pos_users = defaultdict(list)
+    neg_users = defaultdict(list)
     for sub_red in non_endo_subreddits:
-        pos_users, neg_users = getEndoBatchUsers(endo_users, sub_red, start_date, init_date)
+        pos_users, neg_users = getEndoBatchUsers(endo_users, sub_red, start_date, init_date, pos_users, neg_users)
+        print(len(pos_users))
+        print(len(neg_users))
+        print('completed')
 
-def getEndoBatchUsers(endo_users, sub_red, start_date, init_date):
-    users = []
-    body = []
-    d = {}
-   # start_date = datetime.now()
+
+def getEndoBatchUsers(endo_users, sub_red, start_date, init_date,pos_users, neg_users):
+    d = defaultdict(list)
     start_date_epoch = time.mktime(start_date.timetuple())
     end_date = start_date - relativedelta(months=int(1))
     end_date_epoch = time.mktime(end_date.timetuple())
-  #  init_date = parser.parse(init.initDate)
     while end_date > init_date:
          api_comment_url = 'https://api.pushshift.io/reddit/search/comment/?subreddit=' + sub_red + '&before='+ str(int(start_date_epoch)) + '&after=' + str(int(end_date_epoch)) + '&size=5000&sort=desc'
          url = urllib.request.urlopen(api_comment_url)
          user_data = json.loads(url.read().decode())
-         temp_users = [d['author'] for d in user_data['data'] if d['author'] != '[deleted]']
-         users.append(temp_users)
-         temp_body = [d['body'] for d in user_data['data'] if d['author'] != '[deleted]']
-         body.append(temp_body)
-         api_submission_url = 'https://api.pushshift.io/reddit/search/submission/?subreddit=' + sub_red + '&before='+ str(int(start_date_epoch)) + '&after=' + str(int(end_date_epoch)) + '&size=5000&sort=desc'
-         url = urllib.request.urlopen(api_submission_url)
-         user_data = json.loads(url.read().decode())
-         temp_users = [d['author'] for d in user_data['data'] if d['author'] != '[deleted]']
-         users.append(temp_users)
-         temp_body = [d['title'] for d in user_data['data'] if d['author'] != '[deleted]'] # No body available for submission
-         body.append(temp_body)
-      #   d =  dict(zip(set(users),body))
-         d = {k: v for k, v in zip(users[0], body)}
+         for user_detail in user_data['data']:
+             key = user_detail['author']
+             value = user_detail['body']
+             if key in endo_users:
+                 if key in pos_users:
+                     pos_users[key].append(value)
+                 else:
+                     pos_users[key] = [value]
+             else:
+                 if key in neg_users:
+                     neg_users[key].append(value)
+                 else:
+                     neg_users[key] = [value]
          start_date = end_date
          start_date_epoch = end_date_epoch
          end_date = start_date - relativedelta(months=int(1))
          end_date_epoch = time.mktime(end_date.timetuple())
-    return users
+
+    return pos_users, neg_users
 
 Process()
